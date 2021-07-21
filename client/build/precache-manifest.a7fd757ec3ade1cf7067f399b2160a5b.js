7dd9af5afa89364bd3d17efebdbebdd0d9528109
Marco <markforester93@gmail.com>
2021-07-21 21:22:43 +1000

    build

---
+++ client/build/precache-manifest.a7fd757ec3ade1cf7067f399b2160a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c05803b1d7dfd0b4c777efa33755da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1556ad2ea53e02f5c557",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "226fb27ece8d96e8cecb",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "1556ad2ea53e02f5c557",
+    "url": "/static/js/2.b86914e6.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.b86914e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226fb27ece8d96e8cecb",
+    "url": "/static/js/main.fc6e58b3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
